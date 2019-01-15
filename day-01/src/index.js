6419754332c77a3b006899d3ab41bb500f7f08f0
Viktor Barta <barta.viktor.ufs@gmail.com>
2019-01-15 12:19:14 +0100

    feat(task01) init commit setup the base

---
+++ day-01/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('hello')
+
+ReactDOM.render(
+  <h1>Hello, Green Fox Academy!</h1>,
+  document.getElementById('root'),
+);
